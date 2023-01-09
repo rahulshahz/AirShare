d18a7a24429969e58528037d81e44b9040cefcb9
rahulshahz <rahulkshah2002@gmail.com.com>
2023-01-09 17:08:40 +0530

    Initial Commit

--- prac.py
+++
@@ -1,6 +0,0 @@
-import os
-current_directory = os.getcwd()
-current_directory=current_directory+"/static/"
-final_directory = os.path.join(current_directory, 'ne_folder')
-if not os.path.exists(final_directory):
-   os.makedirs(final_directory)
